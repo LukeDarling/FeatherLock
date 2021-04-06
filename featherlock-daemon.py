14ae090e4868f71e2ef51f7df1f4c3c62ff3e9d4
Luke Darling <luke@lukedarling.dev>
2021-04-06 15:23:54 -0400

    Changed Flask server to Bottle server and fixed a couple bugs

--- featherlock-daemon.py
+++ featherlock-daemon.py
@@ -2,23 +2,24 @@
 # FeatherLock written by Luke Darling.
 # All rights reserved.
 
-from flask import Flask, request, jsonify
-import uuid
+from bottle import route, run, template, request
+import uuid, json
 
-app = Flask(__name__, threaded=False)
 lock = {}
 
-@app.route("/", methods=["GET"])
+@route("/")
 def root():
 
-    data = request.args
+    print(lock)
+
+    data = request.query
 
     if "action" in data:
 
         if data["action"] == "lock":
 
             if not "path" in data:
-                return jsonify({"action": "lock", "lock-acquired": False, "message": "No path specified."})
+                return json.dumps({"action": "lock", "lock-acquired": False, "message": "No path specified."})
 
             if "token" in data:
                 token = data["token"]
@@ -32,37 +33,46 @@ def root():
 
             if data["path"] in lock:
                 if lock[data["path"]] == token:
-                    return jsonify({"action": "lock", "path": data["path"], "lock-acquired": True, "token": token, "message": "Lock already acquired."})
+                    return json.dumps({"action": "lock", "path": data["path"], "lock-acquired": True, "token": token, "message": "Lock already acquired."})
                 else:
-                    return jsonify({"action": "lock", "path": data["path"], "lock-acquired": False, "message": "Lock could not be acquired."})
+                    return json.dumps({"action": "lock", "path": data["path"], "lock-acquired": False, "message": "Lock could not be acquired."})
 
             lock[data["path"]] = token;
-            return jsonify({"action": "lock", "path": data["path"], "lock-acquired": True, "token": token, "message": "Lock acquired."})
+            return json.dumps({"action": "lock", "path": data["path"], "lock-acquired": True, "token": token, "message": "Lock acquired."})
 
         elif data["action"] == "unlock":
     
             if not "path" in data:
-                return jsonify({"action": "unlock", "lock-released": False, "message": "No path specified."})
+                return json.dumps({"action": "unlock", "lock-released": False, "message": "No path specified."})
 
             if not "token" in data:
-                return jsonify({"action": "unlock", "path": data["path"], "lock-released": False, "message": "No token specified."})
+                return json.dumps({"action": "unlock", "path": data["path"], "lock-released": False, "message": "No token specified."})
 
             if not data["path"] in lock:
-                return jsonify({"action": "unlock", "path": data["path"], "lock-released": False, "message": "Path not locked."})
+                return json.dumps({"action": "unlock", "path": data["path"], "lock-released": False, "message": "Path not locked."})
 
             if lock[data["path"]] == data["token"]:
                 del lock[data["path"]]
-                return jsonify({"action": "unlock", "path": data["path"], "lock-released": True, "message": "Lock released."})
+                return json.dumps({"action": "unlock", "path": data["path"], "lock-released": True, "message": "Lock released."})
+
+            else:
+                return json.dumps({"action": "unlock", "path": data["path"], "lock-released": False, "message": "Incorrect token."})
 
         elif data["action"] == "status":
         
             if not "path" in data:
-                return jsonify({"action": "status", "message": "No path specified."})
+                return json.dumps({"action": "status", "message": "No path specified."})
 
             if data["path"] in lock:
-                return jsonify({"action": "status", "path": data["path"], "lock-engaged": True, "token": lock[data["path"]], "message": "Lock engaged."})
+                return json.dumps({"action": "status", "path": data["path"], "lock-engaged": True, "token": lock[data["path"]], "message": "Lock engaged."})
 
             else:
-                return jsonify({"action": "status", "path": data["path"], "lock-engaged": False, "message": "Lock disengaged."})
+                return json.dumps({"action": "status", "path": data["path"], "lock-engaged": False, "message": "Lock disengaged."})
 
-app.run("127.0.0.1", 1647)
+        else:
+            return json.dumps({"message": "Please specify a valid action."})
+
+    else:
+        return json.dumps({"message": "Please specify an action."})
+
+run(host="127.0.0.1", port=1647)
